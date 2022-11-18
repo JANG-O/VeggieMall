f934eb793f6da57195ee61a9fac8466a82128e97
minjko <komj36@gmail.com>
2022-11-18 17:22:10 +0900

    create Member Repository v.0

---
+++ src/main/java/com/example/veggiesetmall/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package com.example.veggiesetmall.repository;
+
+import com.example.veggiesetmall.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class) // jpql에 대해 : from 절의 뒤는 Table이 아니라 Entity이다.
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
