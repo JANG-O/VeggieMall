6691967857e791fe8fb030a3c515bac3c3f0f5fb
minjko <komj36@gmail.com>
2022-11-19 19:32:14 +0900

    download Bootstrap v.4.3.1

--- src/main/java/com/example/veggiesetmall/VeggiesetmallApplication.java
+++ src/main/java/com/example/veggiesetmall/VeggiesetmallApplication.java
@@ -2,6 +2,7 @@ package com.example.veggiesetmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class VeggiesetmallApplication {
--- src/test/java/com/example/veggiesetmall/service/MemberServiceTest.java
+++ src/test/java/com/example/veggiesetmall/service/MemberServiceTest.java
@@ -2,7 +2,6 @@ package com.example.veggiesetmall.service;
 
 import com.example.veggiesetmall.domain.Member;
 import com.example.veggiesetmall.repository.MemberRepository;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
